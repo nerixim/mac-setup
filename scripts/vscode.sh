2d108bb0fb66664231382b88bd98c691e902d928
Nikita Kamaev <nikita.kamaev@legalforce.co.jp>
2020-12-06 21:34:16 +0900

    VSCodeとプラグインを追加する

---
+++ scripts/vscode.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash -eux
+
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension shd101wyy.markdown-preview-enhanced
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension eamodio.gitlens
+code --install-extension rebornix.ruby
+code --install-extension sporto.rails-go-to-spec
+code --install-extension karunamurti.rspec-snippets

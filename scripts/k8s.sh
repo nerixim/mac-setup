4adbe73a71b437f97fba9497a1a0d49aebaa93a6
Nikita Kamaev <nikita.kamaev@legalforce.co.jp>
2020-12-09 12:14:35 +0900

    アプリを追加する

--- scripts/k8s.sh
+++ scripts/k8s.sh
@@ -7,3 +7,10 @@ brew install kustomize
 brew install --cask lens
 brew install stern
 brew install k9s
+
+brew install kubectx
+
+# brew tap farmotive/k8s
+# brew install kpoof
+
+brew install derailed/popeye/popeye

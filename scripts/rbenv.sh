f8f348a4a1b01ab8cfb46eddff03754461b43f1b
Nikita Kamaev <nikita.kamaev@legalforce.co.jp>
2021-03-29 23:11:33 +0900

    VSCodeでrbenvを使えるようにする

--- scripts/rbenv.sh
+++ scripts/rbenv.sh
@@ -4,4 +4,6 @@ brew install rbenv
 rbenv init
 
 echo 'eval "$(rbenv init -)"' >> ~/.zsh_profile
+# VSCode-rubyがzshを認識できない
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 # homebrewなどでバイナリーをダウンロードして~/.rbenv/versions/x.x.x/binに配置したらビルドしなくてもよい

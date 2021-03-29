0a301c981b840cad121406c2e4914ddd130a0c3f
Nikita Kamaev <nikita.kamaev@legalforce.co.jp>
2021-03-29 23:10:48 +0900

    プラグインを追加して分類しておく

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -1,10 +1,39 @@
 #!/usr/bin/env bash -eux
 
-code --install-extension ms-azuretools.vscode-docker
 code --install-extension shd101wyy.markdown-preview-enhanced
 code --install-extension davidanson.vscode-markdownlint
+
+code --install-extension ms-azuretools.vscode-docker
 code --install-extension eamodio.gitlens
+code --install-extension codezombiech.gitignore
+
+code --install-extension mrmlnc.vscode-json5
+code --install-extension redhat.vscode-yaml
+code --install-extension mechatroner.rainbow-csv
+code --install-extension mikestead.dotenv
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension 42Crunch.vscode-openapi
+
+code --install-extension wmaurer.change-case
+code --install-extension naumovs.color-highlight
+
+code --install-extension mads-hartmann.bash-ide-vscode
+code --install-extension timonwong.shellcheck
+
 code --install-extension rebornix.ruby
+gem install solargraph
+code --install-extension castwide.solargraph
+code --install-extension misogi.ruby-rubocop
 code --install-extension sporto.rails-go-to-spec
 code --install-extension karunamurti.rspec-snippets
+
 code --install-extension hashicorp.terraform
+
+code --install-extension ms-python.python
+
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension esbenp.prettier-vscode
+code --install-extension GraphQL.vscode-graphql
+code --install-extension jpoissonnier.vscode-styled-components
+code --install-extension stylelint.vscode-stylelint
+code --install-extension octref.vetur

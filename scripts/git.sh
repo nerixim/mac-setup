9a918251156e5c581b6af7ce8519c4e29a4773de
Nikita Kamaev <hiyori.amatsuki@gmail.com>
2022-09-07 18:45:38 +0900

    設定をファイルに切り出して見やすくする

--- scripts/git.sh
+++ scripts/git.sh
@@ -5,25 +5,7 @@ echo 'export PATH=/usr/local/bin/git:$PATH' >> ~/.zshrc
 
 # gh auth login
 
-git config --global user.name "Nikita Kamaev"
-git config --global user.email "hiyori.amatsuki@gmail.com"
-git config --global push.default current
-git config --global remote.pushDefault origin
-git config --global --add --bool push.autoSetupRemote true
-git config --global pull.ff only
-git config --global core.ignorecase false
-git config --global color.ui true
-git config --global core.quotepath false
-
-git config --global alias.st status
-git config --global alias.df diff
-git config --global alias.co commit
-git config --global alias.br branch
-git config --global alias.ps push
-git config --global alias.pl pull
-git config --global alias.ck checkout
-git config --global alias.graph "log --pretty=format:'%Cgreen[%cd] %Cblue%h %Cred<%cn> %Creset%s' --date=short --decorate --graph --branches --tags --remotes"
-git config --global core.excludesFile '~/.gitignore'
+cat ../config/gitconfig >> ~/.gitconfig
 
 cat << 'EOF' >> ~/.aliases
 alias git-prune-merged="git branch --merged | egrep -v '(^\*|master|main|dev|develop)' | xargs git branch -d"

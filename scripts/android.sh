35c82ab66615f27df859fbe4894f0ba439926b95
Nikita Kamaev <hiyori.amatsuki@gmail.com>
2022-10-27 18:24:14 +0900

    asdfのjavaのインストールをする

--- scripts/android.sh
+++ scripts/android.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+asdf plugin-add java https://github.com/halcyon/asdf-java.git
+# 2022/10の時点でv17までしか使えない
+asdf install java openjdk-17.0.2
+asdf global java openjdk-17.0.2
+asdf reshim java
+
 cat << 'EOF' >> ~/.zshrc
 # Android
 export ANDROID_HOME=$HOME/Library/Android/sdk

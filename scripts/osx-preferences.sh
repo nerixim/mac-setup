27ab994e954c4d3ed56ce40e9af35b181b9aaacc
Nikita Kamaev <hiyori.amatsuki@gmail.com>
2023-01-31 14:16:41 +0900

    右クリック対応追加

--- scripts/osx-preferences.sh
+++ scripts/osx-preferences.sh
@@ -21,6 +21,8 @@ defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 
 # Trackpad: map bottom right corner to right-click
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+sudo defaults write com.apple.driver.AppleBluetoothMultitouchTrackpad TrackpadCornerSecondaryClick -int 2
+
 sudo defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 sudo defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 sudo defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1

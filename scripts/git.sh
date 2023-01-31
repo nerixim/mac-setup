49c84a3bb36d2505d58a5bf5272588b73ca4c206
Nikita Kamaev <hiyori.amatsuki@gmail.com>
2023-01-31 14:08:10 +0900

    パスを調整する

--- scripts/git.sh
+++ scripts/git.sh
@@ -5,8 +5,10 @@ echo 'export PATH=/usr/local/bin/git:$PATH' >> ~/.zshrc
 
 # gh auth login
 
-cat ../config/gitconfig >> ~/.gitconfig
-cat ../config/gitignore >> ~/.gitignore
+BASEDIR="$(dirname "$0")/.."
+
+cat ${BASEDIR}/config/gitconfig >> ~/.gitconfig
+cat ${BASEDIR}/config/gitignore >> ~/.gitignore
 
 cat << 'EOF' >> ~/.aliases
 alias git-prune-merged="git branch --merged | egrep -v '(^\*|master|main|dev|develop)' | xargs git branch -d"

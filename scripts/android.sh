b21a583f432cf25b4514a411f36c38f24cd51fc1
Nikita Kamaev <hiyori.amatsuki@gmail.com>
2023-02-20 18:46:56 +0900

    android sdk rootのパスを修正する

--- scripts/android.sh
+++ scripts/android.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-_ANDROID_SDK_VERSION=31
+_ANDROID_SDK_VERSION=33
 _ANDROID_PLATFORM=x86_64
 if [[ $(uname -m) == "arm64" ]]; then
   _ANDROID_PLATFORM=arm64-v8a
@@ -19,6 +19,10 @@ avdmanager create avd --force --name Pixel_6_API_${_ANDROID_SDK_VERSION}_${_ANDR
 
 cat << 'EOF' >> ~/.zshrc
 # Android & React Native
-export ANDROID_SDK_ROOT="/usr/local/bin/sdkmanager"
+export ANDROID_SDK_ROOT="/opt/homebrew/share/android-commandlinetools"
 export ANDROID_HOME=$ANDROID_SDK_ROOT
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
 EOF

b07f7c1b5793c1abb890ab97adb4a67f8495db06
Nikita Kamaev <26106502+nerixim@users.noreply.github.com>
2023-04-21 10:46:15 +0900

    set default rubocop config

--- scripts/asdf.sh
+++ scripts/asdf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash -eux
 
-echo 'source $(brew --prefix asdf)/libexec/asdf.sh' >> "${HOME}/.zsh_profile"
+echo 'source $(brew --prefix asdf)/libexec/asdf.sh' >>"${HOME}/.zsh_profile"
 
 asdf plugin add nodejs || true
 asdf plugin add ruby || true
@@ -15,8 +15,12 @@ asdf plugin-add java https://github.com/halcyon/asdf-java.git || true
 BASEDIR="$(dirname "$0")/"
 cp ${BASEDIR}/.tool-versions "$HOME"
 
-echo 'legacy_version_file = yes' > "$HOME/.asdfrc"
+echo 'legacy_version_file = yes' >"$HOME/.asdfrc"
 
 cp ${BASEDIR}/.default-gems "$HOME"
 cp ${BASEDIR}/.default-npm-packages "$HOME"
 cp ${BASEDIR}/.default-python-packages "$HOME"
+
+# set default rubocop config
+ghq get nerixim/ruby-code-style
+cp ~/ghq/github.com/nerixim/ruby-code-style/.base.rubocop.yml ~/.rubocop.yml

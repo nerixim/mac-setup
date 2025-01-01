95ce703c5704d3f1bed41c8b0485b88862c26a25
Nikita Kamaev <hiyori.amatsuki@gmail.com>
2025-01-01 17:02:48 +0900

    add ghq-fzf

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,16 +1,30 @@
 #!/usr/bin/env bash -eux
 
 # brewでインストールしたgitを使う
-echo 'export PATH=/usr/local/bin/git:$PATH' >> ~/.zshrc
+echo 'export PATH=/usr/local/bin/git:$PATH' >>~/.zshrc
 
 # gh auth login
 
 BASEDIR="$(dirname "$0")/.."
 
-cat ${BASEDIR}/config/gitconfig >> ~/.gitconfig
-cat ${BASEDIR}/config/gitignore >> ~/.gitignore
+cat ${BASEDIR}/config/gitconfig >>~/.gitconfig
+cat ${BASEDIR}/config/gitignore >>~/.gitignore
 
-cat << 'EOF' >> ~/.aliases
+cat <<'EOF' >>~/.aliases
 alias git-prune-merged="git branch --merged | egrep -v '(^\*|master|main|dev|develop)' | xargs git branch -d"
-alias git-pull-recursive="find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=/Users/nikita/github.com/cambia-inc/cambia/backend/{} pull --prune \;"
+alias git-pull-recursive="find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=./{} pull --prune \;"
+EOF
+
+cat <<'EOF' >>~/.aliases
+# ghq
+function ghq-fzf() {
+  local src=$(ghq list | fzf --preview "bat --color=always --style=header,grid --line-range :80 $(ghq root)/{}/README.*")
+  if [ -n "$src" ]; then
+    BUFFER="cd $(ghq root)/$src"
+    zle accept-line
+  fi
+  zle -R -c
+}
+zle -N ghq-fzf
+bindkey '^g' ghq-fzf
 EOF
